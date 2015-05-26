3adb36993ddf4bec6aecba346c3d996542381b3e
Robin Roy Julius <exo_duz@yahoo.com>
2015-05-26 17:00:46 +0800

    Initial Commit

---
+++ LastCigarette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LastCigarette
+//
+//  Created by Robin Roy Julius on 26/05/2015.
+//  Copyright (c) 2015 Robin Roy Julius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
