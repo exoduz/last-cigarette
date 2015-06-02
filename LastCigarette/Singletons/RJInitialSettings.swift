534d06374afc232e35ce40cda25f06d23cd6901f
Robin Roy Julius <exo_duz@yahoo.com>
2015-06-02 15:12:09 +0800

    Created RJSettings file in Singleton to check if app is launched first time or not

---
+++ LastCigarette/Singletons/RJInitialSettings.swift
@@ -0,0 +1,9 @@
+//
+//  RJInitialSettings.swift
+//  LastCigarette
+//
+//  Created by Robin Roy Julius on 2/06/2015.
+//  Copyright (c) 2015 Robin Roy Julius. All rights reserved.
+//
+
+import Foundation
