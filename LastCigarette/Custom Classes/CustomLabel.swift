3c00e05f4b0b5f6f6a93a8a14f593cfb6595f93b
Robin Roy Julius <exo_duz@yahoo.com>
2015-06-19 20:23:21 +0800

    Created CustomLabel subclass

---
+++ LastCigarette/Custom Classes/CustomLabel.swift
@@ -0,0 +1,9 @@
+//
+//  CustomLabel.swift
+//  LastCigarette
+//
+//  Created by Robin Roy Julius on 19/06/2015.
+//  Copyright (c) 2015 Robin Roy Julius. All rights reserved.
+//
+
+import Foundation
